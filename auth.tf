46c45fac168a602200a321d9c4d43ae73413ce0d
Tommy Maintz <tommy.maintz@gmail.com>
2021-02-02 10:47:08 +0100

    Use new login mode to allow login in to the app from Okta

--- auth.tf
+++ auth.tf
@@ -83,6 +83,7 @@ resource "okta_app_oauth" "default" {
   hide_ios                   = var.hide_ios
   hide_web                   = var.hide_web
   login_uri                  = local.login_uri
+  login_mode                 = "SPEC"
   redirect_uris              = [local.redirect_uri]
   response_types             = ["id_token", "code"]
   token_endpoint_auth_method = "client_secret_jwt"
