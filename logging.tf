3bd06d0d0d5716484788587d6d06d85ffff84616
Frank van Boven <frank@cenotaph.nl>
2025-10-24 15:52:30 +0200

    Add access log configuration feature

---
+++ logging.tf
@@ -0,0 +1,35 @@
+resource "aws_cloudwatch_log_delivery_source" "access_logs" {
+  count = var.logging != null ? 1 : 0
+
+  region       = local.global_region
+  name         = var.name
+  log_type     = "ACCESS_LOGS"
+  resource_arn = aws_cloudfront_distribution.default.arn
+  tags         = var.tags
+}
+
+resource "aws_cloudwatch_log_delivery_destination" "access_logs" {
+  count = var.logging != null ? 1 : 0
+
+  region        = local.global_region
+  name          = "${var.name}-s3"
+  output_format = var.logging.output_format
+  tags          = var.tags
+
+  delivery_destination_configuration {
+    destination_resource_arn = var.logging.target_bucket_arn
+  }
+}
+
+resource "aws_cloudwatch_log_delivery" "access_logs" {
+  count = var.logging != null ? 1 : 0
+
+  region                   = local.global_region
+  delivery_source_name     = aws_cloudwatch_log_delivery_source.access_logs[0].name
+  delivery_destination_arn = aws_cloudwatch_log_delivery_destination.access_logs[0].arn
+  tags                     = var.tags
+
+  s3_delivery_configuration {
+    suffix_path = var.logging.target_prefix
+  }
+}
